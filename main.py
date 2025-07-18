2fecb80c1b804be44d47f58ec7b37f45dd960289
Krishna <krishnakumar.kk2409@gmail.com>
2025-07-18 13:12:57 +0530

    krishna code

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ b=2
 c= a*b
 print(c)
 print("hello world")
+
+
+
+print("hello world, this is a test from Krishna")
+print("This is a test for the GitHub Actions workflow.")
+print("This is a test for the GitHub Actions workflow, with a change.")
+
