01118e05dcc6d0ab57ca73dbbd852d80cb9c3729
[muskan] <muskanrajput83647@gmail.com>
2025-07-18 13:13:13 +0530

    add, even, odd,print i

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ b=2
 c= a*b
 print(c)
 print("hello world")
+sum=a+b
+print("The sum of", a, "and", b, "is", sum)
+
+
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+
+for i in range(1, 6):
+    print("Number:", i)
+
